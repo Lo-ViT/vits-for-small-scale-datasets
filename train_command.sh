95ff1591ba592d9cb23fba42b478bfc5f13da236
Long Nguyen <nguyenlo@heracleum.cs.uni-tuebingen.de>
2024-05-27 22:53:37 +0200

    Batch size 256

--- train_command.sh
+++ train_command.sh
@@ -3,6 +3,6 @@ python finetune.py --arch vit  \
                    --name "0003 Larger Batch Size" \
                    --dataset Tiny-Imagenet \
                    --datapath /graphics/scratch2/datasets/tiny-imagenet-200/tiny-imagenet-200 \
-                   --batch_size 512 \
+                   --batch_size 256 \
                    --epochs 100 \
                    --gpu 0
