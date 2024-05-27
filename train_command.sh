7761d05d6b891e00e4c49f8c47b4b3f1459228ed
Long Nguyen <nguyenlo@heracleum.cs.uni-tuebingen.de>
2024-05-27 22:55:51 +0200

    Train command

--- train_command.sh
+++ train_command.sh
@@ -1,11 +1,12 @@
 # TODO: change this to your need
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 python finetune.py --arch vit  \
                    --output_dir /graphics/scratch2/students/nguyenlo \
                    --project "Vit on TinyImageNet" \
-                   --name "0006 Accelerate training of TinyImageNet" \
+                   --name "0007 Multiple GPUs training" \
                    --dataset Tiny-Imagenet \
                    --datapath /graphics/scratch2/students/nguyenlo/tiny-imagenet-200 \
-                   --batch_size 128 \
+                   --batch_size 512 \
                    --epochs 100 \
                    --gpu 0
