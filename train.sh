0898df4e5b4b6b364b8417ea72bc59eacc857929
Long Nguyen <nguyenlo@heracleum.cs.uni-tuebingen.de>
2024-05-27 22:34:06 +0200

    Added train.sh

---
+++ train.sh
@@ -0,0 +1,8 @@
+python finetune.py --arch vit  \
+                   --project "Vit on TinyImageNet" \
+                   --name "0003 Larger Batch Size" \
+                   --dataset Tiny-Imagenet \
+                   --datapath /graphics/scratch2/datasets/tiny-imagenet-200/tiny-imagenet-200 \
+                   --batch_size 512 \
+                   --epochs 100 \
+                   --gpu 0
