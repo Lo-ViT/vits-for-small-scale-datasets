de42dc6e9c50b4c6d42e0c61cf50769614831724
Long Nguyen <long.nguyen@student.uni-tuebingen.de>
2024-05-28 10:12:29 +0200

    0008

--- train_command.sh
+++ train_command.sh
@@ -3,9 +3,9 @@
 python finetune.py --arch vit  \
                    --output_dir /graphics/scratch2/students/nguyenlo \
                    --project "Vit on TinyImageNet" \
-                   --name "0006 Accelerate training of TinyImageNet" \
+                   --name "0008 ViT no ATS no pretraining" \
                    --dataset Tiny-Imagenet \
                    --datapath /graphics/scratch2/students/nguyenlo/tiny-imagenet-200 \
                    --batch_size 128 \
                    --epochs 100 \
-                   --gpu 0
+                   --gpu 0 --nowandb
