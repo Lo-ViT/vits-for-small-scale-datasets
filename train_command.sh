b6b89e9624ab9a3b0d707d1031d965dddc429278
Long Nguyen <long.nguyen@student.uni-tuebingen.de>
2024-05-28 10:43:02 +0200

    gpu4

--- train_command.sh
+++ train_command.sh
@@ -8,4 +8,4 @@ python finetune.py --arch vit  \
                    --datapath /graphics/scratch2/students/nguyenlo/tiny-imagenet-200 \
                    --batch_size 128 \
                    --epochs 100 \
-                   --gpu 0
+                   --gpu 4
