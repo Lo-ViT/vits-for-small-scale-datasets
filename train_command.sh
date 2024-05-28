a8145730d891b99c0d5dd30897960fdc54dae2b9
Long Nguyen <long.nguyen@student.uni-tuebingen.de>
2024-05-28 10:48:35 +0200

    ats

--- train_command.sh
+++ train_command.sh
@@ -1,10 +1,10 @@
 # TODO: change this to your need
 
-python finetune.py --arch vit  \
+python finetune.py --arch vit-ats \
                    --pretrained_weights vit_timnet_patch8_input64.pth \
                    --output_dir /graphics/scratch2/students/nguyenlo \
                    --project "Vit on TinyImageNet" \
-                   --name "0010 ViT no ATS with pretraining" \
+                   --name "0011 ViT ATS with pretraining" \
                    --dataset Tiny-Imagenet \
                    --datapath /graphics/scratch2/students/nguyenlo/tiny-imagenet-200 \
                    --batch_size 128 \
