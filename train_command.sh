bdc06ce50e3f1652fc67a2aefd2caf3f210a1c9f
Long Nguyen <long.nguyen@student.uni-tuebingen.de>
2024-05-28 09:26:18 +0200

    Make train faster

--- train_command.sh
+++ train_command.sh
@@ -2,7 +2,7 @@
 
 python finetune.py --arch vit  \
                    --project "Vit on TinyImageNet" \
-                   --name "0006 In Memory Image Folder" \
+                   --name "0006 Accelerate training of TinyImageNet" \
                    --dataset Tiny-Imagenet \
                    --datapath /graphics/scratch2/students/nguyenlo/tiny-imagenet-200 \
                    --batch_size 128 \
