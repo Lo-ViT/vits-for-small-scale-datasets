c1f862b3c624d525e89c36f426d98f1ee028792e
Long Nguyen <long.nguyen@student.uni-tuebingen.de>
2024-05-28 10:44:04 +0200

    gpu 5

--- train_command.sh
+++ train_command.sh
@@ -1,6 +1,6 @@
 # TODO: change this to your need
 
-python finetune.py --arch vit  \
+python finetune.py --arch vit-ats  \
                    --output_dir /graphics/scratch2/students/nguyenlo \
                    --project "Vit on TinyImageNet" \
                    --name "0009 Vit ATS no pretraining" \
@@ -8,4 +8,4 @@ python finetune.py --arch vit  \
                    --datapath /graphics/scratch2/students/nguyenlo/tiny-imagenet-200 \
                    --batch_size 128 \
                    --epochs 100 \
-                   --gpu 0
+                   --gpu 5
