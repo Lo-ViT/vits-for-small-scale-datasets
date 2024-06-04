65a9a6ea0b5f06aa8955e7d2ea9b551fec15104a
Long Nguyen <long.nguyen@student.uni-tuebingen.de>
2024-06-04 20:49:25 +0200

    Test

--- test_command.sh
+++ test_command.sh
@@ -1,7 +1,7 @@
 # TODO: change this to your need
 
-python test.py --arch vit-ats  \
-                   --model "/graphics/scratch2/students/nguyenlo/CIFAR10/0012 Improve ATS on CIFAR - All ATS/save_finetuned/vit-ats-Base0012 Improve ATS on CIFAR - All ATS--CIFAR10-LR[0.001]-Seed0/best.pth" \
+python test.py --arch vit  \
+                   --model "/graphics/scratch2/students/nguyenlo/CIFAR10/0012 No Layers ATS - drop_tokens=false/save_finetuned/vit-Base0012 No Layers ATS - drop_tokens=false--CIFAR10-LR[0.001]-Seed0/best.pth" \
                    --dataset CIFAR10 \
                    --batch_size 256 \
                    --output_dir /graphics/scratch2/students/nguyenlo/
