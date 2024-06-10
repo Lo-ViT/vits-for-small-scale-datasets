915ccf5eb7899701436a8739225f75d0b1bef1a3
Ece Sena Etoglu <ecesena.etoglu@gmail.com>
2024-06-10 11:40:31 +0200

    feat: created a function to log token outputs in a csv. file. Function indicates all tokens occured in all batches along with model type and batch index,size and image size

--- test.py
+++ test.py
@@ -15,7 +15,9 @@ from finetune import init_parser
 import time
 home = str(Path.home())
 
-def test_model(model, test_loader, criterion, device):
+def test_model(model, test_loader, criterion, device,model_type:str):
+    data_records = [] # list to store records for each batch
+
     model.eval()
     test_loss = 0
     correct = 0
@@ -23,10 +25,15 @@ def test_model(model, test_loader, criterion, device):
     flops = 0
     wall_time = 0.0
     with torch.no_grad():
-        for data, target in test_loader:
+        for batch_idx, (data, target) in enumerate(test_loader):
             data, target = data.to(device), target.to(device)
+            batch_size = data.size(0) # get batch size
+            image_size = data.size(-1) # assuming images are square
+
             before = time.time()
             outputs = model(data)
+            processed_token_list = model.processed_token_list
+
             torch.cuda.synchronize() 
             wall_time += time.time() - before
             flops_dict = flop_count(model, data)
@@ -38,11 +45,30 @@ def test_model(model, test_loader, criterion, device):
             total += target.size(0)
             correct += predicted.eq(target).sum().item()
 
+            # append data for this batch
+            for tokens_processed in processed_token_list:
+                data_records.append({
+                    'Batch': batch_idx,
+                    'Batch Size': batch_size,
+                    'Image Size': image_size,
+                    'Tokens Processed': tokens_processed
+                })
+
+            
+
     avg_loss = test_loss / total
     accuracy = 100. * correct / total
     print(f'Test Loss: {avg_loss:.4f}, Test Accuracy: {accuracy:.2f}%, Test Flops: {flops:.2f}, Test Wall Time: {wall_time:.2f}')
 
+    save_experiment_data_as_csv(data_records,model_type)
+
     return avg_loss, accuracy, flops
+
+"""function to save batch data obt. from test"""
+def save_experiment_data_as_csv(data, filename):
+    df = pd.DataFrame(data)
+    df.to_csv(filename, index=False)
+
 
 def main(args):
     if torch.cuda.is_available() and args.gpu is not None:
@@ -63,7 +89,7 @@ def main(args):
         model.load_state_dict(torch.load(args.model, map_location=device)["model_state_dict"])
 
     # Test model
-    test_model(model, test_loader, criterion, device)
+    test_model(model, test_loader, criterion, device,args.arch)
 
 if __name__ == "__main__":
     parser = init_parser()
